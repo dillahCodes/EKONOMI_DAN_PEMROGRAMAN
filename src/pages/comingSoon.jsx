de1ff2a6e3964426685dd7f5aad8dd2608dbee33
abdillah25 <abijuniansayh@gmail.com>
2023-10-18 21:08:11 +0700

    "ganti image root#

--- src/pages/comingSoon.jsx
+++ src/pages/comingSoon.jsx
@@ -5,7 +5,7 @@ const ComingSoonPage = () => {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen p-5 bg-gray-100">
         <div className="flex flex-col items-center justify-center">
-          <img src="/public/profile.jpg" alt="Logo" className="object-cover w-40 h-40 mb-8 rounded-full" />
+          <img src="/profile.jpg" alt="Logo" className="object-cover w-40 h-40 mb-8 rounded-full" />
           <h1 className="mb-4 text-4xl font-bold text-center">Coming Soon</h1>
           <p className="px-4 mb-8 text-lg text-center md:px-0">I{"'"}m working hard to bring you something awesome. Stay tuned!</p>
           <div className="flex items-center justify-center space-x-4">
