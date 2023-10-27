418131aa5299ebd1b324aafffc661ffe85772bf6
abdillah25 <abijuniansayh@gmail.com>
2023-10-27 13:25:45 +0700

    "change image wodth"

--- src/components/Fragments/HeroSection.jsx
+++ src/components/Fragments/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
   return (
     <section id="home" className="relative min-h-screen mt-5 w-full bg-[#F5F5F5] dark:bg-slate-800">
       <div className="absolute flex flex-row-reverse flex-wrap items-center justify-center w-full px-5 transform -translate-x-1/2 -translate-y-1/2 md:px-10 sm:justify-between top-1/2 left-1/2">
-        <img src="/ekonomi3.png" alt="hero image" className="mx-auto max-w-[350px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[500px] rounded-lg" style={{ width: "500px", height: "auto" }} />
+        <img src="/ekonomi3.png" alt="hero image" width={500} height={"auto"} className="mx-auto max-w-[350px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[500px] rounded-lg" style={{ width: "500px", height: "auto" }} />
         <div className="px-10 mx-auto md:px-5">
           <h1 className="text-2xl font-bold text-black capitalize sm:text-3xl md:text-4xl font-Kanit dark:text-slate-300">Economics and Programming Project</h1>
           <div className="mt-5 text-sm text-black capitalize sm:text-lg font-Oswald dark:text-slate-300">
